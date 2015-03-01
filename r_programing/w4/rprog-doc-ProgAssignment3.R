b78e10d9deccddf6f22243f904fa832818c164e7
limafb <buenofb@hotmail.com>
2015-03-01 07:04:43 -0300

    Added New Files
    Added new files for the Programing Assigmente 3

--- r_programing/w4/rprog-doc-ProgAssignment3.R
+++ r_programing/w4/rprog-doc-ProgAssignment3.R
@@ -1,10 +1,10 @@
 old.wd <- getwd()
-new.wd <- ("D:/Cursos/Coursera/DataScience/R-Programing/W4")
+new.wd <- ("C:/Users/FabioDesk/Documents/GitHub/datasciencecoursera/r_programing/w4")
 setwd(new.wd)
 
 if (!file.exists("data")) {dir.create("data")}
 
-new.wd <- ("D:/Cursos/Coursera/DataScience/R-Programing/W4/data")
+new.wd <- ("C:/Users/FabioDesk/Documents/GitHub/datasciencecoursera/r_programing/w4/data")
 setwd(new.wd)
 
 
@@ -13,7 +13,55 @@ fileUrl <- ("https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2FProgAssignment
 destfile <- ("ProgAssignment3-data.zip")
 
 download.file(fileUrl, destfile, mode = "wb")
-list.files("data")
-list.files()
 
 unzip(destfile, exdir = ".")
+
+list.files()
+
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+str(outcome)
+
+ncol(outcome)
+nrow(outcome)
+names(outcome)
+
+outcome[, 11] <- as.numeric(outcome[, 11])
+## You may get a warning about NAs being introduced; that is okay
+hist(outcome[, 11])
+
+best <- function(state, outcome) {
+    ## Read outcome data
+    outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+      
+    ## Check that state and outcome are valid
+    if(sum(outcome[,7] == state) == 0) {
+        stop("invalid state")
+    }
+    if((!outcome == "heart attack") or (!outcome == "heart failure") or (!outcome == "pneumonia"){
+        stop("invalid outcome")
+    }
+    
+    ## Return hospital name in that state with lowest 30-day death
+    if(outcome == "heart attack"){
+        head(as.numeric(outcome[,13]))
+        str(outcome[,13])
+    }
+    
+    ## rate
+}
+
+#Here is some sample output from the function.
+#> source("best.R")
+#> best("TX", "heart attack")
+#[1] "CYPRESS FAIRBANKS MEDICAL CENTER"
+#> best("TX", "heart failure")
+#[1] "FORT DUNCAN MEDICAL CENTER"
+#> best("MD", "heart attack")
+#[1] "JOHNS HOPKINS HOSPITAL, THE"
+#> best("MD", "pneumonia")
+#[1] "GREATER BALTIMORE MEDICAL CENTER"
+#> best("BB", "heart attack")
+#Error in best("BB", "heart attack") : invalid state
+#> best("NY", "hert attack")
+#Error in best("NY", "hert attack") : invalid outcome
